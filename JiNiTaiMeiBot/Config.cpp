8830de14b39c727d7c64fb96ca7a98af36108b69
KomiMoe <i@komi.moe>
2025-06-20 23:45:20 +0800

    自动序列化

--- JiNiTaiMeiBot/Config.cpp
+++ JiNiTaiMeiBot/Config.cpp
@@ -1,4 +1,4 @@
-﻿#include "Config.h"
+#include "Config.h"
 
 #include <iostream>
 #include <fstream>
@@ -6,97 +6,30 @@
 
 Config* GConfig = nullptr;
 
-#define READ_VALUE(key,ty) \
-    this->##key = [&]() ->auto { \
-        if(configRoot[#key].is##ty()) { \
-            return configRoot[#key].as##ty(); \
-        } \
-        return this->##key; \
-    }();
-
-#define READ_INT(key) READ_VALUE(key, Int);
-#define READ_STRING(key) READ_VALUE(key, String);
-#define READ_BOOL(key) READ_VALUE(key, Bool);
-
-#define WRITE_VALUE(key) \
-    configRoot[#key] = key;
-
-Config::Config(std::string& configFileName) {
-    Json::Reader  reader;
+Config::Config(std::string& configFileName)
+{
     std::ifstream ifs(configFileName);
     Json::Value   configRoot;
 
-    if(!ifs.is_open()) {
-#ifdef _DEBUG
-        WRITE_VALUE(debug);
-
-        WRITE_VALUE(ocrTimeout);
-        WRITE_VALUE(ocrArgs);
-
-        WRITE_VALUE(suspendGTATime);
-        WRITE_VALUE(delaySuspendTime);
-        WRITE_VALUE(checkLoopTime);
-        WRITE_VALUE(matchPanelTimeout);
-        WRITE_VALUE(joiningPlayerKick);
-        WRITE_VALUE(startMatchDelay);
-        WRITE_VALUE(startOnAllJoined);
-        WRITE_VALUE(suspendAfterMatchStarted);
-
-        WRITE_VALUE(pressSTimeStairs);
-        WRITE_VALUE(pressATimeStairs);
-        WRITE_VALUE(goOutStairsTime);
-
-        WRITE_VALUE(intoWallTime);
-
-        WRITE_VALUE(pressSTimeAisle);
-        WRITE_VALUE(pressATimeAisle);
-        WRITE_VALUE(crossAisleTime);
-
-        WRITE_VALUE(pressSTimeGoJob);
-        WRITE_VALUE(pressATimeGoJob);
-        WRITE_VALUE(waitFindJobTimeout);
-
-        Json::StyledStreamWriter writer{};
-        std::ofstream ofs(configFileName);
-        writer.write(ofs, configRoot);
-        ofs.flush();
-        ofs.close();
-        return;
-#endif
-        throw std::runtime_error("Can not open file");
-    }
-    const auto paresResult = reader.parse(ifs, configRoot);
-    ifs.close();
-    if(!paresResult) {
-        throw std::runtime_error(reader.getFormattedErrorMessages());
+    if (ifs.is_open()) {
+        Json::Reader reader;
+        const auto   paresResult = reader.parse(ifs, configRoot);
+        ifs.close();
+        if (!paresResult) {
+            throw std::runtime_error(reader.getFormattedErrorMessages());
+        }
+        for (const auto& [pValue, callback] : readCallbacks) {
+            callback(configRoot, pValue);
+        }
     }
 
-    READ_BOOL(debug);
+    for (const auto& [pValue, callback] : writeCallbacks) {
+        callback(configRoot, pValue);
+    }
 
-    READ_INT(ocrTimeout);
-    READ_STRING(ocrArgs);
-
-    READ_INT(suspendGTATime);
-    READ_INT(delaySuspendTime);
-    READ_INT(checkLoopTime);
-    READ_INT(matchPanelTimeout);
-    READ_INT(joiningPlayerKick);
-    READ_INT(startMatchDelay);
-    READ_BOOL(startOnAllJoined);
-    READ_BOOL(suspendAfterMatchStarted);
-
-    READ_INT(pressSTimeStairs);
-    READ_INT(pressATimeStairs);
-    READ_INT(goOutStairsTime);
-
-    READ_INT(intoWallTime);
-
-    READ_INT(pressSTimeAisle);
-    READ_INT(pressATimeAisle);
-    READ_INT(crossAisleTime);
-
-    READ_INT(pressSTimeGoJob);
-    READ_INT(pressATimeGoJob);
-    READ_INT(waitFindJobTimeout);
-
+    Json::StyledStreamWriter writer{};
+    std::ofstream            ofs(configFileName);
+    writer.write(ofs, configRoot);
+    ofs.flush();
+    ofs.close();
 }
--- JiNiTaiMeiBot/Config.h
+++ JiNiTaiMeiBot/Config.h
@@ -1,42 +1,91 @@
-﻿#pragma once
+#pragma once
 
 #include <string>
 #include <Windows.h>
+#include <json/json.h>
+#include <map>
 
-class Config {
+#define REG_INT_VALUE(name, def) \
+    int name = [&]() -> auto { \
+        readCallbacks[&name] = [](const Json::Value& configRoot, void* pValue) { \
+            if(configRoot[#name].isInt()) { \
+                *static_cast<int*>(pValue) = configRoot[#name].asInt(); \
+            } \
+        }; \
+        writeCallbacks[&name] = [](Json::Value& configRoot, const void* pValue) { \
+            configRoot[#name] = *static_cast<const int*>(pValue); \
+        }; \
+        return def; \
+    }();
+
+#define REG_BOOL_VALUE(name, def) \
+    bool name = [&]() -> auto { \
+        readCallbacks[&name] = [](const Json::Value& configRoot, void* pValue) { \
+            if(configRoot[#name].isBool()) { \
+                *static_cast<bool*>(pValue) = configRoot[#name].asBool(); \
+            } \
+        }; \
+        writeCallbacks[&name] = [](Json::Value& configRoot, const void* pValue) { \
+            configRoot[#name] = *static_cast<const bool*>(pValue); \
+        }; \
+        return def; \
+    }();
+
+#define REG_STRING_VALUE(name, def) \
+    std::string name = [&]() -> auto { \
+        readCallbacks[&name] = [](const Json::Value& configRoot, void* pValue) { \
+            if(configRoot[#name].isString()) { \
+                static_cast<std::string*>(pValue)->clear(); \
+                static_cast<std::string*>(pValue)->append(configRoot[#name].asString());\
+            } \
+        }; \
+        writeCallbacks[&name] = [](Json::Value& configRoot, const void* pValue) { \
+            configRoot[#name] = *static_cast<const std::string*>(pValue); \
+        }; \
+        return def; \
+    }();
+
+
+class Config
+{
+protected:
+    std::map<void*, void (*)(const Json::Value& configRoot, void* pValue)> readCallbacks;
+    std::map<void*, void (*)(Json::Value& configRoot, const void* pValue)> writeCallbacks;
+
 public:
-    bool debug = true;
+    REG_BOOL_VALUE(debug, true);
 
-    int ocrTimeout = 3;
-    std::string ocrArgs = "--models=\".\\models\" "
-            "--det=ch_PP-OCRv4_det_infer.onnx --cls=ch_ppocr_mobile_v2.0_cls_infer.onnx "
-            "--rec=rec_ch_PP-OCRv4_infer.onnx  --keys=dict_chinese.txt --padding=70 "
-            "--maxSideLen=1024 --boxScoreThresh=0.5 --boxThresh=0.3 --unClipRatio=1.6 --doAngle=0 "
-            "--mostAngle=0 --numThread=1";
+    REG_INT_VALUE(ocrTimeout, 3);
+    REG_STRING_VALUE(ocrArgs, "--models=\".\\models\" "
+                     "--det=ch_PP-OCRv4_det_infer.onnx --cls=ch_ppocr_mobile_v2.0_cls_infer.onnx "
+                     "--rec=rec_ch_PP-OCRv4_infer.onnx  --keys=dict_chinese.txt --padding=70 "
+                     "--maxSideLen=1024 --boxScoreThresh=0.5 --boxThresh=0.3 --unClipRatio=1.6 --doAngle=0 "
+                     "--mostAngle=0 --numThread=1");
 
-    int  suspendGTATime = 15;
-    int  delaySuspendTime = 5;
-    int  checkLoopTime = 1;
-    int  matchPanelTimeout = 300;
-    int  joiningPlayerKick = 120;
-    int  startMatchDelay = 15;
-    bool startOnAllJoined = true;
-    bool suspendAfterMatchStarted = true;
-    int exitMatchTimeout = 120;
+    REG_INT_VALUE(suspendGTATime, 15);
 
-    int pressSTimeStairs = 400;
-    int pressATimeStairs = 400;
-    int goOutStairsTime = 3500;
+    REG_INT_VALUE(delaySuspendTime, 5);
+    REG_INT_VALUE(checkLoopTime, 1);
+    REG_INT_VALUE(matchPanelTimeout, 300);
+    REG_INT_VALUE(joiningPlayerKick, 120);
+    REG_INT_VALUE(startMatchDelay, 15);
+    REG_BOOL_VALUE(startOnAllJoined, true);
+    REG_BOOL_VALUE(suspendAfterMatchStarted, true);
+    REG_INT_VALUE(exitMatchTimeout, 120);
 
-    int intoWallTime = 1500;
+    REG_INT_VALUE(pressSTimeStairs, 400);
+    REG_INT_VALUE(pressATimeStairs, 400);
+    REG_INT_VALUE(goOutStairsTime, 3500);
 
-    int pressSTimeAisle = 360;
-    int pressATimeAisle = 500;
-    int crossAisleTime = 4000;
+    REG_INT_VALUE(intoWallTime, 1500);
 
-    int pressSTimeGoJob = 340;
-    int pressATimeGoJob = 480;
-    int waitFindJobTimeout = 15000;
+    REG_INT_VALUE(pressSTimeAisle, 360);
+    REG_INT_VALUE(pressATimeAisle, 500);
+    REG_INT_VALUE(crossAisleTime, 4000);
+
+    REG_INT_VALUE(pressSTimeGoJob, 340);
+    REG_INT_VALUE(pressATimeGoJob, 480);
+    REG_INT_VALUE(waitFindJobTimeout, 15000);
 
     explicit Config(std::string& configFileName);
 };
